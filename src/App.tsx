02013be0d110d117eedcba6d080d9977e7af4218
saifu19 <saifullahpk4@gmail.com>
2024-12-04 18:21:05 +0500

    Router modifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import FacilitatorDashboard from './components/FacilitatorDashboard/FacilitatorDashboard';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { MeetingDashboard } from '@/components/MeetingDashboard/MeetingDashboard';
+import { MeetingPage } from '@/components/MeetingPage/MeetingPage';
+import { MeetingProvider } from '@/components/MeetingContext/MeetingContext';
+import { AgentDashboard } from '@/components/AgentDashboard/AgentDashboard';
+import { MeetingTypesDashboard } from '@/components/MeetingTypesDashboard/MeetingTypesDashboard';
 
 const App: React.FC = () => (
   <ErrorBoundary>
-    <FacilitatorDashboard />
+    <Router>
+      <Routes>
+        {/* Dashboard Route */}
+        <Route path="/" element={<MeetingDashboard />} />
+        <Route path="/agents" element={<AgentDashboard />} />
+        <Route path="/meeting-types" element={<MeetingTypesDashboard />} />
+        {/* Meeting Detail Route */}
+        <Route path="/meeting/:meetingId" element={<MeetingProvider><MeetingPage /></MeetingProvider>} />
+      </Routes>
+    </Router>
   </ErrorBoundary>
 );
 
